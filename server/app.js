a5df08fe2f4c7b0ccd77db33989bed986d598ac0
MustafijurMohan <mustafijurmohan@gmail.com>
2026-05-26 09:45:03 +0600

    add frontend url

--- server/app.js
+++ server/app.js
@@ -41,7 +41,8 @@ app.use(hpp())
 app.use(cors({
 	origin: [
 		"http://localhost:5173",
-		"http://localhost:3000"
+		"http://localhost:3000",
+		"https://next-gadget-one.vercel.app/"
 	],
 	credentials: true,
 	allowedHeaders: ["Content-Type", "Authorization", "token"],
